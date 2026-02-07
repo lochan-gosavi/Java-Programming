4499fae372a1ac3ebe7b6e595134ad456fc59665
lochan-gosavi <lochangosavi05@gmail.com>
2026-02-07 17:19:19 +0530

    first commit

---
+++ basics/CalOfSI.java
@@ -0,0 +1,9 @@
+class CalOfSI{
+    public static void main(String[] args){
+        int p = 1000;
+        int r = 10;
+        int n = 2;
+        int si = (p * r * n) / 100;
+        System.out.println(si);
+    }
+}
