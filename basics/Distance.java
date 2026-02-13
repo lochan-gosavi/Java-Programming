a754e0bd30d56f7b380d62b0c06602b172c4be7c
lochan-gosavi <lochangosavi05@gmail.com>
2026-02-13 17:03:59 +0530

    first commit

---
+++ basics/Distance.java
@@ -0,0 +1,13 @@
+class Distance{
+    public static void main(String[] args){
+        int distanceKM = 1000;
+        int m = distanceKM * 1000;
+        int feet = distanceKM * 3281;
+        int inches = distanceKM * 39370;
+        int cm = distanceKM * 100000;
+        System.out.println(m);
+         System.out.println(feet);
+          System.out.println(inches);
+           System.out.println(cm);
+    }
+}
