c6fbd19d70a08cc5d64d74191d19ee2abb004b5a
lochan-gosavi <lochangosavi05@gmail.com>
2026-02-13 16:39:34 +0530

    first commit

---
+++ basics/CalOfAvg.java
@@ -0,0 +1,9 @@
+class CalOfAvg{
+    public static void main(String args[]){
+        int x = 45;
+        int y = 55;
+        int z = 65;
+        int avg = (x + y + z) / 3;
+        System.out.println(avg);
+    }
+}
