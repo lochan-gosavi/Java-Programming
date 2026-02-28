f4eab5d4c074e91370cc2cec473b8bf7734635df
lochan-gosavi <lochangosavi05@gmail.com>
2026-02-28 18:05:41 +0530

    four

---
+++ functions/LeapYear.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class LeapYear{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        if((year % 400 == 0) || (year % 100 != 0 && year % 4 == 0)){
+            System.out.println("Leap year");
+        }
+        else{
+            System.out.println("Not a leap year");
+        }
+        sc.close();
+    }
+}
