c7371f0e14493464487e5e62797216efcb6d2550
lochan-gosavi <lochangosavi05@gmail.com>
2026-02-28 18:12:11 +0530

    four

---
+++ functions/Product.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Product{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int p = prod(a, b);
+        System.out.println(p);
+    }
+    static int prod(int x, int y){
+        return x * y;
+    }
+}
