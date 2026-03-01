974b8177b666a08c044c978c05216caaac2bb82b
lochan-gosavi <lochangosavi05@gmail.com>
2026-03-01 15:27:58 +0530

    two

---
+++ practical_clg/BasicInfo.java
@@ -0,0 +1,6 @@
+//print basic info like name, class, add, city
+class basicInfo{
+    public static void main(String[] args){
+        System.out.printf("Name : Lochan Gosavi\n\nClass: S.Y.C.S.E\n\nAdd : A/P Pandur;\nTal : Kudal\nDist. : Sindhudurg\nCity : Kudal");
+    }
+}
