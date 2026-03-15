382fbe16bd330902158d959aaa87702a4467faac
lochan-gosavi <lochangosavi05@gmail.com>
2026-03-15 09:35:25 +0530

    four

---
+++ _5_Arrays/SortArray.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class SortArray{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        int i;
+        for(i = 0; i < arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        for(i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("yes");
+    }
+}
